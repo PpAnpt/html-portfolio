ee73b392b2d03f2a822de668eeb464f766b19b75
PpAnpt <ppanapat1208@gmail.com>
2024-10-29 17:31:01 +0700

    middleware.js

--- middleware.js
+++ middleware.js
@@ -1,33 +1,14 @@
-// import { next } from '@vercel/edge';
+import { next } from '@vercel/edge';
 
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-// export default function middleware(req) {
-//   return next({
-//     headers: {
-//       'Referrer-Policy': 'origin-when-cross-origin',
-//       'X-Frame-Options': 'DENY',
-//       'X-Content-Type-Options': 'nosniff',
-//       'X-DNS-Prefetch-Control': 'on',
-//       'Strict-Transport-Security':
-//         'max-age=31536000; includeSubDomains; preload',
-//     },
-//   });
-// }
-
-window.onscroll = () => {
-    sections.forEach(sec => {
-        let top = window.scrollY;
-        let offset = sec.offsetTop -150;
-        let height = sec.offsetHeight;
-        let id = sec.getAttribute('id');
-
-        if(top >= offset && top < offset + height) {
-            navLinks.forEach(links =>{
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        };
-    }); 
-};
+export default function middleware(req) {
+  return next({
+    headers: {
+      'Referrer-Policy': 'origin-when-cross-origin',
+      'X-Frame-Options': 'DENY',
+      'X-Content-Type-Options': 'nosniff',
+      'X-DNS-Prefetch-Control': 'on',
+      'Strict-Transport-Security':
+        'max-age=31536000; includeSubDomains; preload',
+    },
+  });
+}
